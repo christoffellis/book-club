2ac8ac4149ef624e4b1874788d7a2edd2089487c
Christo Kruger <christo.kruger@bbd>
2024-09-13 21:58:56 +0200

    more publish config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-    // other configuration options
-    rules: {
-      'no-unused-vars': ['warn'] // Change 'error' to 'warn'
-    }
-  };
-  
+  // other configuration options
+  rules: {
+    'no-unused-vars': ['warn'], // Change 'error' to 'warn'
+    'react-hooks/exhaustive-deps': ['warn'], // Change 'error' to 'warn'
+    // Add or adjust other rules as needed
+  }
+};
